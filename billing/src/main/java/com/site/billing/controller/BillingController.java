3b846aed9bdbe839ad784d56681326c6fd737008
kevin.0213(김시원)/dkt <kevin.0213@kakaocorp.com>
2025-07-14 06:48:31 +0900

    1. application 설정파일 추가 (eureka 세팅)

--- billing/src/main/java/com/site/billing/controller/BillingController.java
+++ billing/src/main/java/com/site/billing/controller/BillingController.java
@@ -1,4 +1,12 @@
 package com.site.billing.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BillingController {
+    @GetMapping("/")
+    public String index() {
+        return "billing";
+    }
 }
